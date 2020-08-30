ef7340f8abcd470d2731306403b8146bcaf7a15b
kevin <kevin@betacode.io>
2020-08-30 23:07:20 +0800

    change text

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React is awesome
-        </p>
-        <p>
-          Docker is awesome too
-        </p>
+        <p>React is awesome</p>
+        <p>Docker is awesome too</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
